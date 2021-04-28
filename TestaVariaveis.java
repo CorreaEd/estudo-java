abd6f1f45cf747ef1dd97c9f260ae8b8d9125020
Eduardo Corrêa <44485065+CorreaEd@users.noreply.github.com>
2021-04-28 02:20:50 -0300

    add TestaVariaveis.java

---
+++ TestaVariaveis.java
@@ -0,0 +1,23 @@
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Ola novo teste!");
+		
+		int idade;
+		idade = 25;
+		
+		System.out.println(idade);
+		
+		idade = 25 + 10;
+		
+		System.out.println(idade);
+		
+		idade = (5 * 4) + 5;
+		
+		System.out.println(idade);
+		
+
+		System.out.println("A idade é " + idade + ", parabéns!");
+	}
+}
