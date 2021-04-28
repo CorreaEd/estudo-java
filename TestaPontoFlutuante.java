e19343f02208467e9e0727c917bb772a3608dd8a
Eduardo Corrêa <44485065+CorreaEd@users.noreply.github.com>
2021-04-28 02:32:53 -0300

    add TestaPontoFlutuante.java

---
+++ TestaPontoFlutuante.java
@@ -0,0 +1,24 @@
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args) {
+		double salario;
+		salario = 1250.70;
+		System.out.println("Meu salário é " + salario);
+		
+		double idade = 25;
+		System.out.println("Minha idade é " + idade);
+		
+		double divisao = 3.14 / 2;
+		System.out.println("A divisão de um pi (3,14) por 2 é " + divisao);
+		
+		int outraDivisao = 5 / 2;
+		System.out.println(outraDivisao);
+		
+		double novaTentativa = 5 / 2;
+		System.out.println(novaTentativa);
+		
+		double outraTentativa = 5.0 / 2;
+		System.out.println(outraTentativa);
+	}
+
+}
